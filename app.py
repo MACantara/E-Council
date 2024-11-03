0815638df845f954665728438ab9bccf59a3cb72
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-11-03 14:19:33 +0800

    Add models for signatories, attendees and meeting documentation

--- app.py
+++ app.py
@@ -281,6 +281,47 @@ class MinutesOfTheMeeting(db.Model):
     def __repr__(self):
         return f'<MinutesOfTheMeeting {self.minutes_of_the_meeting_id}: {self.minutes_of_the_meeting_agenda}>'
 
+class Signatories(db.Model):
+    __tablename__ = 'signatories'
+
+    signatory_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    signatory_title = db.Column(db.String(50), nullable=False)
+    signatory_first_name = db.Column(db.String(50), nullable=False)
+    signatory_middle_name = db.Column(db.String(50), nullable=True)
+    signatory_last_name = db.Column(db.String(50), nullable=False)
+    signatory_suffix = db.Column(db.String(10), nullable=True)
+    signatory_position = db.Column(db.String(100), nullable=False)
+    signatory_department = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Signatories {self.signatory_id}: {self.signatory_first_name} {self.signatory_last_name}>'
+
+class MinutesOfTheMeetingPhotoDocumentation(db.Model):
+    __tablename__ = 'minutes_of_the_meeting_photo_documentation'
+
+    minutes_of_the_meeting_photo_documentation_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    minutes_of_the_meeting_id = db.Column(db.Integer, db.ForeignKey('minutes_of_the_meeting.minutes_of_the_meeting_id'), nullable=False)
+    minutes_of_the_meeting_photo_documentation_url = db.Column(db.String(255), nullable=False)
+    minutes_of_the_meeting_photo_documentation_description = db.Column(db.Text, nullable=True)
+
+    minutes_of_the_meeting = db.relationship('MinutesOfTheMeeting', foreign_keys=[minutes_of_the_meeting_id])
+
+    def __repr__(self):
+        return f'<MinutesOfTheMeetingPhotoDocumentation {self.minutes_of_the_meeting_photo_documentation_id}: {self.minutes_of_the_meeting_photo_documentation_url}>'
+
+class MinutesOfTheMeetingAttendees(db.Model):
+    __tablename__ = 'minutes_of_the_meeting_attendees'
+
+    minutes_of_the_meeting_attendees_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    minutes_of_the_meeting_id = db.Column(db.Integer, db.ForeignKey('minutes_of_the_meeting.minutes_of_the_meeting_id'), nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.users_id'), nullable=False)
+
+    minutes_of_the_meeting = db.relationship('MinutesOfTheMeeting', foreign_keys=[minutes_of_the_meeting_id])
+    user = db.relationship('Users', foreign_keys=[users_id])
+
+    def __repr__(self):
+        return f'<MinutesOfTheMeetingAttendees {self.minutes_of_the_meeting_attendees_id}: Meeting {self.minutes_of_the_meeting_id}, User {self.users_id}>'
+
 # Custom Jinja2 Filters
 def truncate_text(text, length=100):
     if len(text) > length:
